796b584888aed79656a66dd76570a786cf6c33f0
Anirudha Acharya <127017012+anirudhaacharyap@users.noreply.github.com>
2026-05-03 01:54:41 +0530

    chore: remove flutter files from backend repository

--- android/app/src/main/kotlin/com/forge/forge_app/MainActivity.kt
+++
@@ -1,5 +0,0 @@
-package com.forge.forge_app
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity : FlutterActivity()
--- test/widget_test.dart
+++
@@ -1,9 +0,0 @@
-import 'package:flutter_test/flutter_test.dart';
-import 'package:forge_app/main.dart';
-
-void main() {
-  testWidgets('App smoke test', (WidgetTester tester) async {
-    await tester.pumpWidget(const ForgeApp());
-    expect(find.text('FORGE'), findsOneWidget);
-  });
-}
